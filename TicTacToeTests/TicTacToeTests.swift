ec3a9e4a6659b4030175504a613c5df866be0297
AndrealHarrison <andrealharrison@gmail.com>
2023-05-12 18:35:26 -0700

    Refactor

--- TicTacToeTests/TicTacToeTests.swift
+++ TicTacToeTests/TicTacToeTests.swift
@@ -100,9 +100,9 @@ class TicViewModelTests: XCTestCase {
         ticModel.setCell(n: 3, c: .o)
 
         // Assert
-        XCTAssertTrue(ticModel.grid[3] == Cell.x)
-        XCTAssertEqual((ticModel.grid.filter { $0 == Cell.x }.count), 1)
-        XCTAssertEqual((ticModel.grid.filter { $0 == Cell.b }.count), 8)
+        XCTAssertTrue(ticModel.grid[3] == cell.x)
+        XCTAssertEqual((ticModel.grid.filter { $0 == cell.x }.count), 1)
+        XCTAssertEqual((ticModel.grid.filter { $0 == cell.b }.count), 8)
     }
 
     func test_setCellBlank_ignored() {
@@ -114,9 +114,9 @@ class TicViewModelTests: XCTestCase {
         ticModel.setCell(n: 3, c: .b)
 
         // Assert
-        XCTAssertTrue(ticModel.grid[3] == Cell.x)
-        XCTAssertEqual((ticModel.grid.filter { $0 == Cell.x }.count), 1)
-        XCTAssertEqual((ticModel.grid.filter { $0 == Cell.b }.count), 8)
+        XCTAssertTrue(ticModel.grid[3] == cell.x)
+        XCTAssertEqual((ticModel.grid.filter { $0 == cell.x }.count), 1)
+        XCTAssertEqual((ticModel.grid.filter { $0 == cell.b }.count), 8)
     }
 
 
