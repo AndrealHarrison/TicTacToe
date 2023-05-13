622ceeb0ef2f3466d75471b1bbaee35437c76a12
AndrealHarrison <andrealharrison@gmail.com>
2023-05-12 17:57:04 -0700

    Green - Updated model to pass test

---
+++ TicTacToe/Model.swift
@@ -0,0 +1,30 @@
+//
+//  Model.swift
+//  TicTacToe
+//
+//  Created by Andrea Harrison on 5/12/23.
+//
+
+import Foundation
+
+enum cell: String {
+    case x = "X"
+    case o = "O"
+    case b = "" // blank
+}
+
+struct TicModel {
+    private var _grid: [cell]
+    
+    init() {
+        _grid = []
+        for _ in 0..<9 {
+            _grid.append(cell.x)
+        }
+    }
+    
+    var grid: [cell] {
+        get { _grid }
+    }
+
+}
