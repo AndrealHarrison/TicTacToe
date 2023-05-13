1be46e4ee204ea0503321c19dc24a32b309298e5
AndrealHarrison <andrealharrison@gmail.com>
2023-05-12 17:42:52 -0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Andrea Harrison on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
